adaf7d71870f43b823d5259dc916fe2ea37f1781
zhanba <c5e1856@gmail.com>
2022-02-13 23:34:16 +0800

    fix: update blog

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './footer.css'
 
-export const Footer = () => (
-  <footer className="footer">
-    <div className="footer-content">©2018 ryannz</div>
-  </footer>
-)
+export const Footer = () => {
+  const year = new Date().getFullYear()
+  return (
+    <footer className="footer">
+      <div className="footer-content">©{year} ryannz</div>
+    </footer>
+  )
+}
